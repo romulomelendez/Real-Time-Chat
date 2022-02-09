25368c26c3bbf48272ccf30eaa742a1c0ec2531a
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2022-02-09 14:43:49 -0300

    creating a container element with styled-components

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,17 +4,19 @@ import { Select } from '../../components/Select'
 import { UserName } from '../../components/UserName'
 import { Button } from '../../components/Button'
 
+import { Container } from './style'
+
 export const Home: React.FC = () => {
 
     return (
 
-        <>
+        <Container>
 
             <Select />
             <UserName />
             <Button />
         
-        </>
+        </Container>
 
     )
 
