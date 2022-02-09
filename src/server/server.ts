fe847af6feb171ca5d9f1ae048052cf070579872
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2022-02-09 16:29:02 -0300

    removing old configurations server.ts

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,13 +1,3 @@
-import express from 'express'
-import http from 'http'
-import { Server } from 'socket.io'
 
-
-const app = express()
-
-const httpServer = http.createServer(app)
-const io = new Server(httpServer)
-
-app.use()
 
 app.listen(3000, () => console.log('App running on port 3000'))
