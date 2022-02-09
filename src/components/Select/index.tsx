75c3dc130c675bbc0a748a66d83f4bcf7d1fc92f
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2022-02-09 12:06:40 -0300

    implementing Select component

---
+++ src/components/Select/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+//import {  } from './style'
+
+export const Select: React.FC = () => (
+
+    <select name = "chat-class">
+
+        <option value = "java">Java</option>
+        <option value = "php">PHP</option>
+        <option value = "react.js">React.js</option>
+        <option value = "node.js">Node.js</option>    
+
+    </select>
+    
+)
