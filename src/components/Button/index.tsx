daa99b9a8acbf6e7eb799f0ab551d362ba615fe3
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2022-02-09 11:46:08 -0300

    implementing Button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,7 @@
+import { Btn } from './style'
+
+export const Button: React.FC = () => (
+
+    <Btn type="submit">GO</Btn>
+    
+)
---
+++ src/components/Button/style.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const Btn = styled.button `
+
+    cursor: pointer;
+
+` 
