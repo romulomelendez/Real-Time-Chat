7376787e71b40bf8ed6726222c0a22229c2aa31d
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2022-02-09 15:14:12 -0300

    Select component style update

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
-//import {  } from './style'
+import { Choose } from './style'
 
 export const Select: React.FC = () => (
 
-    <select name = "chat-class">
+    <Choose name = "chat-class">
 
         <option value = "java">Java</option>
         <option value = "php">PHP</option>
         <option value = "react.js">React.js</option>
         <option value = "node.js">Node.js</option>    
 
-    </select>
+    </Choose>
     
 )
---
+++ src/components/Select/style.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Choose = styled.select `
+
+    font-size: 15pt;
+    background-color: #EFEFEF;
+    border: 6px solid transparent;
+    border-color: #fff transparent transparent transparent;
+
+`
