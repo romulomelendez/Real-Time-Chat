fdba74043833575d07006394bb7a680d81612f52
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2022-02-09 16:11:00 -0300

    Home page style update

--- src/pages/Home/style.js
+++ src/pages/Home/style.js
@@ -2,14 +2,19 @@ import styled from 'styled-components'
 
 export const Container = styled.div `
 
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    gap: 20px;
     margin: 10px;
     padding: 10px;
     height: 200px;
     width: 200px;
+
+    form {
+      
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        gap: 20px;
+    
+    }
 
 `
