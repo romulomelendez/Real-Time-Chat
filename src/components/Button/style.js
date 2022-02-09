2a85eec2e6d138f4e9284b461992bc3b17a37c9e
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2022-02-09 15:08:06 -0300

    Button component style update

--- src/components/Button/style.js
+++ src/components/Button/style.js
@@ -2,6 +2,23 @@ import styled from 'styled-components'
 
 export const Btn = styled.button `
 
+    font-size: 12pt;
+    font-weight: bold;
+    width: 150px;
+    border-radius: 4px;
+    border: none;
+    margin: 10px;
+    padding: 10px;
     cursor: pointer;
+    color: grey;
+    background-color: #EFEFEF;
+    transition-duration: 0.2s;
+
+    : hover {
+
+        color: white;
+        background-color: #c6c2c2;
+
+    }
 
 ` 
