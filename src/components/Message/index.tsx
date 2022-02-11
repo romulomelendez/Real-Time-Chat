c894a39684d7611a4c56c7ee96f88621d70fb134
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2022-02-11 17:39:51 -0300

    creating a Message component

---
+++ src/components/Message/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import { SMS, MessageDate } from './style'
+
+export const Message: React.FC = () => {
+
+    return (
+
+        <div>
+
+            <SMS>Message</SMS>
+            <MessageDate>Data</MessageDate>
+
+        </div>
+
+    )
+
+}
