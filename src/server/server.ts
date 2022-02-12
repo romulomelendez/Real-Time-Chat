16f672a348d0caf69f1eb974c763a849fbf70a19
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2022-02-11 23:43:03 -0300

    removing server files to implement another one

--- src/server/server.ts
+++
@@ -1,6 +0,0 @@
-import { httpServer } from './http'
-
-const HOST = 'localhost'
-const PORT = process.env.PORT || 3001
-
-httpServer.listen(PORT, () => console.log(`App's running on ${HOST}:${PORT}`))
--- src/server/websocket.ts
+++
@@ -1,9 +0,0 @@
-import { io } from './http'
-
-io.on('connection', socket => {
-
-    console.log('Connected')
-    console.log(socket.id)
-    socket.on('disconnect', () => console.log('Disconnected'))
-
-})
